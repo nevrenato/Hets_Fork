b845f46d6659410018db756244aabfaf1c505624
Renato Neves <nevrenato@gmail.com>
2012-11-23 17:57:11 +0000

    completed the parser with the right precedence rules and associativity

--- TopHybrid/Parse_AS.hs
+++ TopHybrid/Parse_AS.hs
@@ -59,52 +59,56 @@ ids :: AParser st [SIMPLE_ID]
 ids = sepBy simpleId anSemiOrComma 
  
 formParser :: AnyLogic -> AParser st Form_Wrapper 
-formParser (Logic l) = (fParser' l) >>= return . Form_Wrapper 
+formParser (Logic l) = (topParser l) >>= return . Form_Wrapper 
 
-op :: AParser st ((TH_FORMULA f) -> (TH_FORMULA f) -> (TH_FORMULA f))
-op = 
-        (asKey "/\\" >> return Conjunction )
-        <|>
-        (asKey "\\/" >> return Disjunction)
-        <|>
-        (asKey "->" >> return Implication)
-        <|>
-        (asKey "<->" >> return BiImplication)
-     
-fParser' :: (Sentences l f sign morphism symbol) => l -> AParser st (TH_FORMULA f)
-fParser' l = chainl1 (fParser l) op >>= return 
+-- BinaryOps parsers, the reason to separate them, is so we can get a 
+-- precedence order
+conjP :: AParser st ((TH_FORMULA f) -> (TH_FORMULA f) -> (TH_FORMULA f))
+conjP = asKey "/\\" >> return Conjunction
+
+disjP :: AParser st ((TH_FORMULA f) -> (TH_FORMULA f) -> (TH_FORMULA f))
+disjP = asKey "\\/" >> return Disjunction
+
+impAndBiP :: AParser st ((TH_FORMULA f) -> (TH_FORMULA f) -> (TH_FORMULA f))
+impAndBiP = (asKey "->" >> return Implication) <|> (asKey "<->" >> return BiImplication)
+--
+-- The parsing goes by order of precedence
+topParser :: (Sentences l f sign morphism symbol) => l -> AParser st (TH_FORMULA f)
+topParser l = chainl1 fp1 impAndBiP >>= return
+        where   fp1 = (chainl1 fp2 disjP >>= return)
+                fp2 = (chainl1 (fParser l) conjP >>= return) 
 
 fParser :: (Sentences l f sign morphism symbol) => l -> AParser st (TH_FORMULA f)
 fParser l  =
         do 
         asKey "("
-        f <- fParser' l
+        f <- (topParser l)
         asKey ")"
         return f
         <|>
         do
         asKey "not"
-        f <- fParser' l
+        f <- (fParser l <|> topParser l)
         return $ Neg f
         <|> 
         do
         asKey "@"
         n <- simpleId
-        f <- (fParser l <|> fParser' l)
+        f <- (fParser l <|> topParser l)
         return $ At n f 
         <|>
         do 
         asKey "["
         m <- simpleId
         asKey "]"
-        f <- fParser' l
+        f <- (fParser l <|> topParser l)
         return $ Box m f
         <|>
         do 
         asKey "<"
         m <- simpleId
         asKey ">"
-        f <- fParser' l
+        f <- ( fParser l <|> topParser l)
         return $ Dia m f
         <|>
         do 
